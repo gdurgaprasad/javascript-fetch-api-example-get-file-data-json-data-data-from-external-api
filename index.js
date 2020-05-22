1ea0f22c280768042057185a2415c5c909fb9cf2
sujatha <durgaprasaddp184@gmail.com>
2020-05-22 11:29:23 +0530

    code reformat

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
 const outputDiv = document.getElementById("output");
 
-button1.addEventListener("click", getFileData);
-button2.addEventListener("click", getJsonData);
-button3.addEventListener("click", getApiData);
+loadEventListeners();
+
+function loadEventListeners() {
+  button1.addEventListener("click", getFileData);
+  button2.addEventListener("click", getJsonData);
+  button3.addEventListener("click", getApiData);
+}
 
 function getFileData() {
   fetch("data.txt")
-    .then((data) => data.text())
-    .then((res) => {
+    .then(data => data.text())
+    .then(res => {
       outputDiv.innerHTML = res;
     })
-    .catch((error) => {
+    .catch(error => {
       outputDiv.innerHTML = error;
     });
 }
@@ -21,16 +25,16 @@ function getFileData() {
 function getJsonData() {
   let output = "";
   fetch("data.json")
-    .then((data) => data.json())
-    .then((res) => {
-      res.forEach((item) => {
+    .then(data => data.json())
+    .then(res => {
+      res.forEach(item => {
         output += `<li>${item.color} - ${item.value} </li>`;
       });
     })
     .then(() => {
       outputDiv.innerHTML = output;
     })
-    .catch((error) => {
+    .catch(error => {
       outputDiv.innerHTML = error;
     });
 }
@@ -38,16 +42,16 @@ function getJsonData() {
 function getApiData() {
   let output = "";
   fetch("https://jsonplaceholder.typicode.com/users")
-    .then((data) => data.json())
-    .then((response) => {
-      response.forEach((item) => {
+    .then(data => data.json())
+    .then(response => {
+      response.forEach(item => {
         output += `<li>${item.name}</li>`;
       });
     })
     .then(() => {
       outputDiv.innerHTML = output;
     })
-    .catch((error) => {
+    .catch(error => {
       outputDiv.innerHTML = error;
     });
 }
